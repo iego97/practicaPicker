8d114bf0eb53e4a4ca8706b7740c8f7efd24a961
Alumno <ulsa@VictorLP.local>
2018-09-13 13:36:27 -0700

    practica

--- practicaPicker/ViewController.swift
+++ practicaPicker/ViewController.swift
@@ -8,11 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    let categorias = ["Animales Salvajes","Granja","transporte", "instrumento", "Colores", "Morado"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return categorias.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return categorias[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch row{
+            
+        case 0:
+              view.backgroundColor = UIColor.red
+        case 1:
+            imageView.image = UIImage(named: "farm.jpg")
+        case 2:
+            imageView.image = UIImage(named: "colores.jpg")
+    
+    
+            
+            
+        default:
+            break
+        }
+    
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        view.backgroundColor = UIColor.red
+        
     }
 
     override func didReceiveMemoryWarning() {
